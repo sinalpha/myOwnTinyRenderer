ff64da1d14de1a40113449018a66709aee74bbd7
sinalpha <hanhdhx65@gmail.com>
2026-07-16 00:53:23 +0900

    Draw triangles by using thread - bbox method.

--- main.cpp
+++ main.cpp
@@ -1,111 +1,89 @@
 #include <cmath>
 #include <tuple>
-#include <algorithm>
-#include <array>
-
 #include "geometry.h"
 #include "model.h"
 #include "tgaimage.h"
 
+constexpr int width = 900;
+constexpr int height = 900;
 
-constexpr int width  = 128;
-constexpr int height = 128;
+constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
+constexpr TGAColor green = { 0, 255,   0, 255 };
+constexpr TGAColor red = { 0,   0, 255, 255 };
+constexpr TGAColor blue = { 255, 128,  64, 255 };
+constexpr TGAColor yellow = { 0, 200, 255, 255 };
 
-constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
-constexpr TGAColor green   = {  0, 255,   0, 255};
-constexpr TGAColor red     = {  0,   0, 255, 255};
-constexpr TGAColor blue    = {255, 128,  64, 255};
-constexpr TGAColor yellow  = {  0, 200, 255, 255};
-
-void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
-    bool steep = std::abs(ax-bx) < std::abs(ay-by);
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
     if (steep) { // if the line is steep, we transpose the image
         std::swap(ax, ay);
         std::swap(bx, by);
     }
-    if (ax>bx) { // make it left−to−right
+    if (ax > bx) { // make it left−to−right
         std::swap(ax, bx);
         std::swap(ay, by);
     }
     int y = ay;
     int ierror = 0;
-    for (int x=ax; x<=bx; x++) {
+    for (int x = ax; x <= bx; x++) {
         if (steep) // if transposed, de−transpose
             framebuffer.set(y, x, color);
         else
             framebuffer.set(x, y, color);
-        ierror += 2 * std::abs(by-ay);
+        ierror += 2 * std::abs(by - ay);
         if (ierror > bx - ax) {
             y += by > ay ? 1 : -1;
-            ierror -= 2 * (bx-ax);
+            ierror -= 2 * (bx - ax);
         }
     }
 }
 
-void scanline(int ax, int ay, int bx, int by, int cx, int cy, TGAImage& framebuffer, TGAColor color) {
+std::tuple<int, int> project(vec3 v) { // First of all, (x,y) is an orthogonal projection of the vector (x,y,z).
+    return { (v.x + 1.) * width / 2,   // Second, since the input models are scaled to have fit in the [-1,1]^3 world coordinates,
+             (v.y + 1.) * height / 2 }; // we want to shift the vector (x,y) and then scale it to span the entire screen.
+}
 
-    //min x point, max x point.
-    //min y point, max y point.
-    struct vec2 {
-        int x;
-        int y;
-    };
+double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
+    return .5 * ((by - ay) * (bx + ax) + (cy - by) * (cx + bx) + (ay - cy) * (ax + cx));
+}
 
-    std::array<vec2, 3> pts;
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage& framebuffer, TGAColor color) {
+    int bbminx = std::min(std::min(ax, bx), cx); // bounding box for the triangle
+    int bbminy = std::min(std::min(ay, by), cy); // defined by its top left and bottom right corners
+    int bbmaxx = std::max(std::max(ax, bx), cx);
+    int bbmaxy = std::max(std::max(ay, by), cy);
+    double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
 
-    pts.at(0) = { ax, ay };
-    pts.at(1) = { bx, by };
-    pts.at(2) = { cx, cy };
-
-    std::sort(pts.begin(), pts.end(), [](const vec2& a, const vec2& b) {
-        return a.y < b.y;
-        });
-
-    vec2& minYpt = pts.at(0);
-    vec2& midYpt = pts.at(1);
-    vec2& maxYpt = pts.at(2);
-
-    for (int y = minYpt.y; y <= maxYpt.y; ++y) {
-
-        int startX = minYpt.x;
-        int endX = minYpt.x;
-
-        if (y < midYpt.y) {
-            startX = (midYpt.y == minYpt.y) ? midYpt.x :
-                minYpt.x + (int)((midYpt.x - minYpt.x) * ((float)(y - minYpt.y) / (midYpt.y - minYpt.y)));
+#pragma omp parallel for
+    for (int x = bbminx; x <= bbmaxx; x++) {
+        for (int y = bbminy; y <= bbmaxy; y++) {
+            double alpha = signed_triangle_area(x, y, bx, by, cx, cy) / total_area;
+            double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
+            double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
+            if (alpha < 0 || beta < 0 || gamma < 0) continue; // negative barycentric coordinate => the pixel is outside the triangle
+            framebuffer.set(x, y, color);
         }
-        else {
-            startX = (maxYpt.y == midYpt.y) ? midYpt.x :
-                midYpt.x + (int)((maxYpt.x - midYpt.x) * ((float)(y - midYpt.y) / (maxYpt.y - midYpt.y)));
-        }
-
-        if (maxYpt.y != minYpt.y) {
-            float t2 = (float)(y - minYpt.y) / (maxYpt.y - minYpt.y);
-            endX = minYpt.x + (int)((maxYpt.x - minYpt.x) * t2);
-        }
-        if (startX > endX) std::swap(startX, endX);
-
-        line(startX, y, endX, y, framebuffer, color);
-
     }
-
 }
-
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
-    line(ax, ay, bx, by, framebuffer, color);
-    line(bx, by, cx, cy, framebuffer, color);
-    line(cx, cy, ax, ay, framebuffer, color);
-    scanline(ax, ay, bx, by, cx, cy, framebuffer, color);
-}
-
-
 
 int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return 1;
+    }
+
+    Model model(argv[1]);
     TGAImage framebuffer(width, height, TGAImage::RGB);
-    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
-    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
-    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
+
+    for (int i = 0; i < model.nfaces(); i++) { // iterate through all triangles
+        auto [ax, ay] = project(model.vert(i, 0));
+        auto [bx, by] = project(model.vert(i, 1));
+        auto [cx, cy] = project(model.vert(i, 2));
+        TGAColor rnd;
+        for (int c = 0; c < 3; c++) rnd[c] = std::rand() % 255;
+        triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
+    }
+
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
-
