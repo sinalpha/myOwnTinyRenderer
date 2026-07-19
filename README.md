957b5254380b7e663ac62663c9e408fe88b791b1
sinalpha <hanhdhx65@gmail.com>
2026-07-20 01:04:58 +0900

    revisit md file.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # myOwnTinyRenderer   
 implemention of https://haqr.eu/tinyrenderer/
+
+<img src="depth-map.png" width="200px" height="200px">
--- geometry.h
+++ geometry.h
@@ -3,22 +3,45 @@
 #include <cassert>
 #include <iostream>
 
-template<int n> struct vec {
-    double data[n] = {0};
-    double& operator[](const int i)       { assert(i>=0 && i<n); return data[i]; }
-    double  operator[](const int i) const { assert(i>=0 && i<n); return data[i]; }
+template<int n>
+struct vec {
+    double data[n] = { 0 };
+    double& operator[](const int i) { assert(i >= 0 && i < n); return data[i]; }
+    double  operator[](const int i) const { assert(i >= 0 && i < n); return data[i]; }
+    vec& operator+(const vec& v) { 
+        return *this; }
 };
 
-template<int n> std::ostream& operator<<(std::ostream& out, const vec<n>& v) {
+template<int n>
+std::ostream& operator<<(std::ostream& out, const vec<n>& v) {
     for (int i=0; i<n; i++) out << v[i] << " ";
     return out;
 }
 
-template<> struct vec<3> {
+template<>
+struct vec<4> {
+    double x = 0, y = 0, z = 0;
+    double& operator[](const int i) { assert(i >= 0 && i < 4); return i ? (1 == i ? y : z) : x; }
+    double  operator[](const int i) const { assert(i >= 0 && i < 4); return i ? (1 == i ? y : z) : x; }
+};
+
+template<>
+struct vec<3> {
     double x = 0, y = 0, z = 0;
     double& operator[](const int i)       { assert(i>=0 && i<3); return i ? (1==i ? y : z) : x; }
     double  operator[](const int i) const { assert(i>=0 && i<3); return i ? (1==i ? y : z) : x; }
 };
 
+template<>
+struct vec<2> {
+    double x = 0, y = 0, z = 0;
+    double& operator[](const int i) { assert(i >= 0 && i < 2); return i ? (1 == i ? y : z) : x; }
+    double  operator[](const int i) const { assert(i >= 0 && i < 2); return i ? (1 == i ? y : z) : x; }
+};
+
+
+typedef vec<4> vec4;
 typedef vec<3> vec3;
+typedef vec<2> vec2;
+
 
