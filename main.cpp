0d3af35315b17f6f4b24aff4bbab904b64931bf6
sinalpha <hanhdhx65@gmail.com>
2026-07-27 23:35:33 +0900

    synchronize

--- main.cpp
+++ main.cpp
@@ -1,93 +1,64 @@
-#include <cstdlib>
-#include <random>
-
 #include "our_gl.h"
 #include "model.h"
 
-extern mat<4, 4> ModelView, Perspective; // "OpenGL" state matrices and
+extern mat<4,4> ModelView, Perspective; // "OpenGL" state matrices and
 extern std::vector<double> zbuffer;     // the depth buffer
 
-struct RandomShader : IShader {
-    const Model& model;
-    TGAColor color = {};
-    vec3 tri[3];  // triangle in eye coordinates
-    vec3 normal;
-    vec3 light;
+struct PhongShader : IShader {
+    const Model &model;
+    vec3 l;          // light direction in eye coordinates
+    vec3 tri[3];     // triangle in eye coordinates
 
-    RandomShader(const Model& m) : model(m) {
+    PhongShader(const vec3 light, const Model &m) : model(m) {
+        l = normalized((ModelView*vec4{light.x, light.y, light.z, 0.}).xyz()); // transform the light vector to view coordinates
     }
 
     virtual vec4 vertex(const int face, const int vert) {
         vec3 v = model.vert(face, vert);                          // current vertex in object coordinates
-        vec4 gl_Position = ModelView * vec4{ v.x, v.y, v.z, 1. };
+        vec4 gl_Position = ModelView * vec4{v.x, v.y, v.z, 1.};
         tri[vert] = gl_Position.xyz();                            // in eye coordinates
         return Perspective * gl_Position;                         // in clip coordinates
     }
 
-    virtual void calculateNormal() {
-        vec3 ab = tri[1] - tri[0];
-        vec3 ac = tri[2] - tri[0];
-
-        normal = cross(ab, ac);
-        normal = normal / norm(normal);
-    }
-
-    virtual void calculateLight(const vec3 light_) {
-        light = normalized(light_);
-    }
-
-    virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const {
-
-        const vec3 r = 2 * normal * (normal * light) - light;
-        const vec3 v = normalized(-bar.x * tri[0] + -bar.y * tri[1] + -bar.z * tri[2]);
-        const double ambient = 0.1;
-        const double diffuse = std::max(0., normal * light);
-        const double specular = std::max(0., r * v);
-
-        double intensity = std::min(1., ambient + diffuse + specular);
-        vec3 sum = vec3{ intensity, intensity, intensity } * 255.;
-
-        TGAColor res = TGAColor{
-            static_cast<unsigned char>(sum.x),
-            static_cast<unsigned char>(sum.y),
-            static_cast<unsigned char>(sum.z),
-            255 };
-        return { false,  res };// do not discard the pixel
+    virtual std::pair<bool,TGAColor> fragment(const vec3 bar) const {
+        TGAColor gl_FragColor = {255, 255, 255, 255};             // output color of the fragment
+        vec3 n = normalized(cross(tri[1]-tri[0], tri[2]-tri[0])); // triangle normal in eye coordinates
+        vec3 r = normalized(n * (n * l)*2 - l);                   // reflected light direction
+        double ambient = .3;                                      // ambient light intensity
+        double diff = std::max(0., n * l);                        // diffuse light intensity
+        double spec = std::pow(std::max(r.z, 0.), 35);            // specular intensity, note that the camera lies on the z-axis (in eye coordinates), therefore simple r.z, since (0,0,1)*(r.x, r.y, r.z) = r.z
+        for (int channel : {0,1,2})
+            gl_FragColor[channel] *= std::min(1., ambient + .4*diff + .9*spec);
+        return {false, gl_FragColor};                             // do not discard the pixel
     }
 };
 
 int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return 1;
+    }
 
-    constexpr int width = 800;      // output image size
+    constexpr int width  = 800;      // output image size
     constexpr int height = 800;
-    constexpr vec3    eye{ -1, 0, 2 }; // camera position
-    constexpr vec3 center{ 0, 0, 0 }; // camera direction
-    constexpr vec3     up{ 0, 1, 0 }; // camera up vector
+    constexpr vec3  light{ 1, 1, 1}; // light source
+    constexpr vec3    eye{-1, 0, 2}; // camera position
+    constexpr vec3 center{ 0, 0, 0}; // camera direction
+    constexpr vec3     up{ 0, 1, 0}; // camera up vector
 
     lookat(eye, center, up);                                   // build the ModelView   matrix
-    init_perspective(norm(eye - center));                        // build the Perspective matrix
-    init_viewport(width / 16, height / 16, width * 7 / 8, height * 7 / 8); // build the Viewport    matrix
+    init_perspective(norm(eye-center));                        // build the Perspective matrix
+    init_viewport(width/16, height/16, width*7/8, height*7/8); // build the Viewport    matrix
     init_zbuffer(width, height);
-    TGAImage framebuffer(width, height, TGAImage::RGB, { 177, 195, 209, 255 });
+    TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    std::mt19937 rng(std::random_device{}());
-    std::uniform_int_distribution<int> dist(0, 254);
-
-
-    for (int m = 1; m < 2; m++) {                    // iterate through all input objects
-        Model model("D:/programming/myOwnTinyRenderer/obj/diablo3_pose.obj");
-        RandomShader shader(model);
-        for (int f = 0; f < model.nfaces(); f++) {      // iterate through all facets
-            shader.color = { static_cast<unsigned char>(std::rand() % 255),
-                             static_cast<unsigned char>(std::rand() % 255),
-                             static_cast<unsigned char>(std::rand() % 255),
-                             255 };
+    for (int m=1; m<argc; m++) {                    // iterate through all input objects
+        Model model(argv[m]);                       // load the data
+        PhongShader shader(light, model);
+        for (int f=0; f<model.nfaces(); f++) {      // iterate through all facets
             Triangle clip = { shader.vertex(f, 0),  // assemble the primitive
                               shader.vertex(f, 1),
                               shader.vertex(f, 2) };
-            shader.calculateNormal();
-            //the sun light always locate in origin of xy and z = 10;
-            shader.calculateLight(vec3(0.f, 0.f, 10.f));
             rasterize(clip, shader, framebuffer);   // rasterize the primitive
         }
     }
@@ -95,3 +66,4 @@ int main(int argc, char** argv) {
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
+
--- tgaimage.cpp
+++ tgaimage.cpp
@@ -2,9 +2,9 @@
 #include <cstring>
 #include "tgaimage.h"
 
-TGAImage::TGAImage(const int w, const int h, const int bpp, TGAColor c) : w(w), h(h), bpp(bpp), data(w* h* bpp, 0) {
-    for (int j = 0; j < h; j++)
-        for (int i = 0; i < w; i++)
+TGAImage::TGAImage(const int w, const int h, const int bpp, TGAColor c) : w(w), h(h), bpp(bpp), data(w*h*bpp, 0) {
+    for (int j=0; j<h; j++)
+        for (int i=0; i<w; i++)
             set(i, j, c);
 }
 
@@ -16,34 +16,32 @@ bool TGAImage::read_tga_file(const std::string filename) {
         return false;
     }
     TGAHeader header;
-    in.read(reinterpret_cast<char*>(&header), sizeof(header));
+    in.read(reinterpret_cast<char *>(&header), sizeof(header));
     if (!in.good()) {
         std::cerr << "an error occured while reading the header\n";
         return false;
     }
-    w = header.width;
-    h = header.height;
-    bpp = header.bitsperpixel >> 3;
-    if (w <= 0 || h <= 0 || (bpp != GRAYSCALE && bpp != RGB && bpp != RGBA)) {
+    w   = header.width;
+    h   = header.height;
+    bpp = header.bitsperpixel>>3;
+    if (w<=0 || h<=0 || (bpp!=GRAYSCALE && bpp!=RGB && bpp!=RGBA)) {
         std::cerr << "bad bpp (or width/height) value\n";
         return false;
     }
-    size_t nbytes = bpp * w * h;
+    size_t nbytes = bpp*w*h;
     data = std::vector<std::uint8_t>(nbytes, 0);
-    if (3 == header.datatypecode || 2 == header.datatypecode) {
-        in.read(reinterpret_cast<char*>(data.data()), nbytes);
+    if (3==header.datatypecode || 2==header.datatypecode) {
+        in.read(reinterpret_cast<char *>(data.data()), nbytes);
         if (!in.good()) {
             std::cerr << "an error occured while reading the data\n";
             return false;
         }
-    }
-    else if (10 == header.datatypecode || 11 == header.datatypecode) {
+    } else if (10==header.datatypecode||11==header.datatypecode) {
         if (!load_rle_data(in)) {
             std::cerr << "an error occured while reading the data\n";
             return false;
         }
-    }
-    else {
+    } else {
         std::cerr << "unknown file format " << (int)header.datatypecode << "\n";
         return false;
     }
@@ -51,14 +49,14 @@ bool TGAImage::read_tga_file(const std::string filename) {
         flip_vertically();
     if (header.imagedescriptor & 0x10)
         flip_horizontally();
-    std::cerr << w << "x" << h << "/" << bpp * 8 << "\n";
+    std::cerr << w << "x" << h << "/" << bpp*8 << "\n";
     return true;
 }
 
-bool TGAImage::load_rle_data(std::ifstream& in) {
-    size_t pixelcount = w * h;
+bool TGAImage::load_rle_data(std::ifstream &in) {
+    size_t pixelcount = w*h;
     size_t currentpixel = 0;
-    size_t currentbyte = 0;
+    size_t currentbyte  = 0;
     TGAColor colorbuffer;
     do {
         std::uint8_t chunkheader = 0;
@@ -67,35 +65,34 @@ bool TGAImage::load_rle_data(std::ifstream& in) {
             std::cerr << "an error occured while reading the data\n";
             return false;
         }
-        if (chunkheader < 128) {
+        if (chunkheader<128) {
             chunkheader++;
-            for (int i = 0; i < chunkheader; i++) {
-                in.read(reinterpret_cast<char*>(colorbuffer.bgra), bpp);
+            for (int i=0; i<chunkheader; i++) {
+                in.read(reinterpret_cast<char *>(colorbuffer.bgra), bpp);
                 if (!in.good()) {
                     std::cerr << "an error occured while reading the header\n";
                     return false;
                 }
-                for (int t = 0; t < bpp; t++)
+                for (int t=0; t<bpp; t++)
                     data[currentbyte++] = colorbuffer.bgra[t];
                 currentpixel++;
-                if (currentpixel > pixelcount) {
+                if (currentpixel>pixelcount) {
                     std::cerr << "Too many pixels read\n";
                     return false;
                 }
             }
-        }
-        else {
+        } else {
             chunkheader -= 127;
-            in.read(reinterpret_cast<char*>(colorbuffer.bgra), bpp);
+            in.read(reinterpret_cast<char *>(colorbuffer.bgra), bpp);
             if (!in.good()) {
                 std::cerr << "an error occured while reading the header\n";
                 return false;
             }
-            for (int i = 0; i < chunkheader; i++) {
-                for (int t = 0; t < bpp; t++)
+            for (int i=0; i<chunkheader; i++) {
+                for (int t=0; t<bpp; t++)
                     data[currentbyte++] = colorbuffer.bgra[t];
                 currentpixel++;
-                if (currentpixel > pixelcount) {
+                if (currentpixel>pixelcount) {
                     std::cerr << "Too many pixels read\n";
                     return false;
                 }
@@ -106,9 +103,9 @@ bool TGAImage::load_rle_data(std::ifstream& in) {
 }
 
 bool TGAImage::write_tga_file(const std::string filename, const bool vflip, const bool rle) const {
-    constexpr std::uint8_t developer_area_ref[4] = { 0, 0, 0, 0 };
-    constexpr std::uint8_t extension_area_ref[4] = { 0, 0, 0, 0 };
-    constexpr std::uint8_t footer[18] = { 'T','R','U','E','V','I','S','I','O','N','-','X','F','I','L','E','.','\0' };
+    constexpr std::uint8_t developer_area_ref[4] = {0, 0, 0, 0};
+    constexpr std::uint8_t extension_area_ref[4] = {0, 0, 0, 0};
+    constexpr std::uint8_t footer[18] = {'T','R','U','E','V','I','S','I','O','N','-','X','F','I','L','E','.','\0'};
     std::ofstream out;
     out.open(filename, std::ios::binary);
     if (!out.is_open()) {
@@ -116,23 +113,22 @@ bool TGAImage::write_tga_file(const std::string filename, const bool vflip, cons
         return false;
     }
     TGAHeader header = {};
-    header.bitsperpixel = bpp << 3;
-    header.width = w;
+    header.bitsperpixel = bpp<<3;
+    header.width  = w;
     header.height = h;
-    header.datatypecode = (bpp == GRAYSCALE ? (rle ? 11 : 3) : (rle ? 10 : 2));
+    header.datatypecode = (bpp==GRAYSCALE ? (rle?11:3) : (rle?10:2));
     header.imagedescriptor = vflip ? 0x00 : 0x20; // top-left or bottom-left origin
-    out.write(reinterpret_cast<const char*>(&header), sizeof(header));
+    out.write(reinterpret_cast<const char *>(&header), sizeof(header));
     if (!out.good()) goto err;
     if (!rle) {
-        out.write(reinterpret_cast<const char*>(data.data()), w * h * bpp);
+        out.write(reinterpret_cast<const char *>(data.data()), w*h*bpp);
         if (!out.good()) goto err;
-    }
-    else if (!unload_rle_data(out)) goto err;
-    out.write(reinterpret_cast<const char*>(developer_area_ref), sizeof(developer_area_ref));
+    } else if (!unload_rle_data(out)) goto err;
+    out.write(reinterpret_cast<const char *>(developer_area_ref), sizeof(developer_area_ref));
     if (!out.good()) goto err;
-    out.write(reinterpret_cast<const char*>(extension_area_ref), sizeof(extension_area_ref));
+    out.write(reinterpret_cast<const char *>(extension_area_ref), sizeof(extension_area_ref));
     if (!out.good()) goto err;
-    out.write(reinterpret_cast<const char*>(footer), sizeof(footer));
+    out.write(reinterpret_cast<const char *>(footer), sizeof(footer));
     if (!out.good()) goto err;
     return true;
 err:
@@ -140,21 +136,21 @@ err:
     return false;
 }
 
-bool TGAImage::unload_rle_data(std::ofstream& out) const {
+bool TGAImage::unload_rle_data(std::ofstream &out) const {
     const std::uint8_t max_chunk_length = 128;
-    size_t npixels = w * h;
+    size_t npixels = w*h;
     size_t curpix = 0;
-    while (curpix < npixels) {
-        size_t chunkstart = curpix * bpp;
-        size_t curbyte = curpix * bpp;
+    while (curpix<npixels) {
+        size_t chunkstart = curpix*bpp;
+        size_t curbyte = curpix*bpp;
         std::uint8_t run_length = 1;
         bool raw = true;
-        while (curpix + run_length < npixels && run_length < max_chunk_length) {
+        while (curpix+run_length<npixels && run_length<max_chunk_length) {
             bool succ_eq = true;
-            for (int t = 0; succ_eq && t < bpp; t++)
-                succ_eq = (data[curbyte + t] == data[curbyte + t + bpp]);
+            for (int t=0; succ_eq && t<bpp; t++)
+                succ_eq = (data[curbyte+t]==data[curbyte+t+bpp]);
             curbyte += bpp;
-            if (1 == run_length)
+            if (1==run_length)
                 raw = !succ_eq;
             if (raw && succ_eq) {
                 run_length--;
@@ -165,39 +161,39 @@ bool TGAImage::unload_rle_data(std::ofstream& out) const {
             run_length++;
         }
         curpix += run_length;
-        out.put(raw ? run_length - 1 : run_length + 127);
+        out.put(raw ? run_length-1 : run_length+127);
         if (!out.good()) return false;
-        out.write(reinterpret_cast<const char*>(data.data() + chunkstart), (raw ? run_length * bpp : bpp));
+        out.write(reinterpret_cast<const char *>(data.data()+chunkstart), (raw?run_length*bpp:bpp));
         if (!out.good()) return false;
     }
     return true;
 }
 
 TGAColor TGAImage::get(const int x, const int y) const {
-    if (!data.size() || x < 0 || y < 0 || x >= w || y >= h) return {};
-    TGAColor ret = { 0, 0, 0, 0, bpp };
-    const std::uint8_t* p = data.data() + (x + y * w) * bpp;
-    for (int i = bpp; i--; ret.bgra[i] = p[i]);
+    if (!data.size() || x<0 || y<0 || x>=w || y>=h) return {};
+    TGAColor ret = {0, 0, 0, 0, bpp};
+    const std::uint8_t *p = data.data()+(x+y*w)*bpp;
+    for (int i=bpp; i--; ret.bgra[i] = p[i]);
     return ret;
 }
 
-void TGAImage::set(int x, int y, const TGAColor& c) {
-    if (!data.size() || x < 0 || y < 0 || x >= w || y >= h) return;
-    memcpy(data.data() + (x + y * w) * bpp, c.bgra, bpp);
+void TGAImage::set(int x, int y, const TGAColor &c) {
+    if (!data.size() || x<0 || y<0 || x>=w || y>=h) return;
+    memcpy(data.data()+(x+y*w)*bpp, c.bgra, bpp);
 }
 
 void TGAImage::flip_horizontally() {
-    for (int i = 0; i < w / 2; i++)
-        for (int j = 0; j < h; j++)
-            for (int b = 0; b < bpp; b++)
-                std::swap(data[(i + j * w) * bpp + b], data[(w - 1 - i + j * w) * bpp + b]);
+    for (int i=0; i<w/2; i++)
+        for (int j=0; j<h; j++)
+            for (int b=0; b<bpp; b++)
+                std::swap(data[(i+j*w)*bpp+b], data[(w-1-i+j*w)*bpp+b]);
 }
 
 void TGAImage::flip_vertically() {
-    for (int i = 0; i < w; i++)
-        for (int j = 0; j < h / 2; j++)
-            for (int b = 0; b < bpp; b++)
-                std::swap(data[(i + j * w) * bpp + b], data[(i + (h - 1 - j) * w) * bpp + b]);
+    for (int i=0; i<w; i++)
+        for (int j=0; j<h/2; j++)
+            for (int b=0; b<bpp; b++)
+                std::swap(data[(i+j*w)*bpp+b], data[(i+(h-1-j)*w)*bpp+b]);
 }
 
 int TGAImage::width() const {
@@ -207,3 +203,4 @@ int TGAImage::width() const {
 int TGAImage::height() const {
     return h;
 }
+
