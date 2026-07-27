c57d034e8217826d6c0e2a74ade977b896f8a592
sinalpha <hanhdhx65@gmail.com>
2026-07-27 22:38:53 +0900

    apply phong shading.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ The code of [tinyrenderer](https://haqr.eu/tinyrenderer/) for learning low-level
 <figure>
     <img src="depth-map.png" width="200px" height="200px">
     <figcaption>Result: This image visualizes Z-values of freagments.</figcaption>
-        <img src="rotate-object.png" width="200px" height="200px">
+    <img src="rotate-object.png" width="200px" height="200px">
     <figcaption>Result: The rotated object by linear transform.</figcaption>
+        <img src="phong-shading.png" width="200px" height="200px">
+    <figcaption>Result: The phong shaded image..</figcaption>
 </figure>
--- tgaimage.h
+++ tgaimage.h
@@ -21,28 +21,28 @@ struct TGAHeader {
 #pragma pack(pop)
 
 struct TGAColor {
-    std::uint8_t bgra[4] = {0,0,0,0};
+    std::uint8_t bgra[4] = { 0,0,0,0 };
     std::uint8_t bytespp = 4;
     std::uint8_t& operator[](const int i) { return bgra[i]; }
+    const std::uint8_t& operator[](const int i) const { return bgra[i]; }
 };
 
 struct TGAImage {
-    enum Format { GRAYSCALE=1, RGB=3, RGBA=4 };
+    enum Format { GRAYSCALE = 1, RGB = 3, RGBA = 4 };
     TGAImage() = default;
-    TGAImage(const int w, const int h, const int bpp);
+    TGAImage(const int w, const int h, const int bpp, TGAColor c = {});
     bool  read_tga_file(const std::string filename);
-    bool write_tga_file(const std::string filename, const bool vflip=true, const bool rle=true) const;
+    bool write_tga_file(const std::string filename, const bool vflip = true, const bool rle = true) const;
     void flip_horizontally();
     void flip_vertically();
     TGAColor get(const int x, const int y) const;
-    void set(const int x, const int y, const TGAColor &c);
+    void set(const int x, const int y, const TGAColor& c);
     int width()  const;
     int height() const;
 private:
-    bool   load_rle_data(std::ifstream &in);
-    bool unload_rle_data(std::ofstream &out) const;
+    bool   load_rle_data(std::ifstream& in);
+    bool unload_rle_data(std::ofstream& out) const;
     int w = 0, h = 0;
     std::uint8_t bpp = 0;
     std::vector<std::uint8_t> data = {};
 };
-
